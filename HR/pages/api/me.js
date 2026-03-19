f9bb66207fd2dae2ebb97226fde1ede8ab341a0f
Daryl50 <142259591+Daryl50@users.noreply.github.com>
2026-03-19 00:02:02 +0000

    Delete HR directory

--- HR/pages/api/me.js
+++
@@ -1,8 +0,0 @@
-const { requireAuth } = require('../../lib/middleware')
-
-export default async function handler(req, res) {
-  const user = await requireAuth(req, res)
-  if (!user) return
-  // return the token payload (id, email, role)
-  res.json({ user })
-}
